2f8e0874a91dee68d1c3113a0472b4c50b2761b2
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-02-18 23:18:09 +0530

    Getting words

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
+  const [answer, setAnswer] = useState(null);
+  useEffect(() => {
+    async function getAnswers() {
+      const res = await fetch("http://localhost:8080/solutions");
+      const data = await res.json();
+      const randomAnswer = data[Math.floor(Math.random() * data.length)];
+      setAnswer(randomAnswer.word);
+    }
+    getAnswers();
+  }, [setAnswer]);
+
   return (
     <div>
       <h1>Wordle</h1>
+      {answer && <div>Solution is: {answer}</div>}
     </div>
   );
 }
